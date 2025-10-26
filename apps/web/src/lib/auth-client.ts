358f41755c2acc9e2ec5d91b311c3d846613518a
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-10-26 02:08:29 -0700

    chore(web): update auth-client import and variable name

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,8 +1,8 @@
-import { createAuthClient } from "better-auth/client";
 import { inferAdditionalFields } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
 import { env } from "../env";
 
-export const auth_client = createAuthClient({
+export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_API_URL, // The base URL for the API (where auth-server is running)
   plugins: [inferAdditionalFields()],
 });
