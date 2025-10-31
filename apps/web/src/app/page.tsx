1cde9d80ecd4cddc48cdb1c98600bd95329f9a99
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-10-31 15:04:11 -0700

    refactor(web): rename sign-in-out-button file path for consistency

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { authClient } from "../lib/auth-client";
-import SignInOutButton from "./(components)/sign-in-out-button";
+import SignInOutButton from "./_components/sign-in-out-button";
 
 export default function Home() {
   const { data: session } = authClient.useSession();
