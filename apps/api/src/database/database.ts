51078e8b2a3b56ed617225cf018a8c7ab0856184
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-01 16:31:25 -0700

    feat(api): integrate mongoose-zod with database setup

--- apps/api/src/database/database.ts
+++ apps/api/src/database/database.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+import { setup } from "mongoose-zod";
+import { z } from "zod";
 import { env } from "../env";
 import { logger } from "../libs/logger.ts";
 
+setup({ z }); // mongoose-zod will add new functions to the prototype of `z`
 await mongoose.connect(env.DATABASE_URL);
 
 export const databaseConnection = mongoose.connection;
