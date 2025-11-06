016f10e35b29eb2757b7f756a0a46726f8f24ea9
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-06 12:22:10 -0800

    feat(api): add create and delete post handlers with validation

--- apps/api/src/core/post/post-router.ts
+++ apps/api/src/core/post/post-router.ts
@@ -1,10 +1,31 @@
+import { NotFoundError } from "elysia";
+import { v4 as uuidv4 } from "uuid";
 import { logger } from "../../libs/logger.ts";
 import { protectedProcedure } from "../../libs/orpc.ts";
+import { UserCollection } from "../users/user-schema.ts";
+import { PostCollection } from "./post-schema.ts";
 
 export const postRouter = {
-  createPost: protectedProcedure.posts.createPost.handler(({ input }) => {
-    logger.info(`Received input: ${input.id}`);
-    return input.id;
+  createPost: protectedProcedure.posts.createPost.handler(async ({ input }) => {
+    const userDocument = await UserCollection.findOne({ email: input.createdBy });
+    if (!userDocument) {
+      throw new NotFoundError(`User not found with email: ${input.createdBy}`);
+    }
+    const id = uuidv4();
+
+    await PostCollection.insertOne({
+      id,
+      ...input,
+    });
+
+    return { id };
   }),
-  test: protectedProcedure.posts.test.handler(() => "test"),
+
+  deletePost: protectedProcedure.posts.deletePost.handler(({ input }) => {
+    logger.info("hello from deletePost!");
+
+    return {
+      success: true,
+    };
+  }),
 };
