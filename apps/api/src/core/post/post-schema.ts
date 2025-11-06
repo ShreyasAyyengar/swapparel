f9e79d65bf64bca6ee2929db5ae2e46367705ad9
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-06 12:21:24 -0800

    fix(api): adjust post schema to ensure Mongoose compatibility with Zod types

--- apps/api/src/core/post/post-schema.ts
+++ apps/api/src/core/post/post-schema.ts
@@ -1,13 +1,18 @@
 import { createPostInput } from "@swapparel/contracts";
 import { toMongooseSchema } from "mongoose-zod";
+import { z } from "zod";
 import { databaseConnection } from "../../database/database.ts";
 
 const PostSchema = toMongooseSchema(
-  createPostInput.mongoose({
-    schemaOptions: {
-      collection: "posts",
-    },
-  })
+  createPostInput
+    // certain 'Zod Types' (z.url, z.email) must be redefined as more primitive types to serialise with Mongoose
+    .omit({ createdBy: true, images: true })
+    .extend({ createdBy: z.string(), images: z.array(z.string()) })
+    .mongoose({
+      schemaOptions: {
+        collection: "posts",
+      },
+    })
 );
 
 export const PostCollection = databaseConnection.model("posts", PostSchema, "posts");
