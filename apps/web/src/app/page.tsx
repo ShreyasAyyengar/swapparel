0ea02f9a325fd9221aa2dfe374a163621436a7fb
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-05 22:47:49 -0800

    refactor(web): remove unused SignInOutButton component from Home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import Navbar from "./_components/navbar";
-import SignInOutButton from "./_components/sign-in-out-button";
 
 export default function Home() {
   return (
     <div className={"flex h-[2000px] items-center justify-center p-8 align-middle"}>
       <p className="top-0 font-bold text-xl">Swapparel</p>
       <Navbar />
-      {/*<p className="font-bold font-mono text-4xl">Swapparel</p>*/}
-      <SignInOutButton />
     </div>
   );
 }
