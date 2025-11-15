04ccd54e310454ac1a16411383c00daccb916061
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-14 23:53:07 -0800

    feat(web): add query state for peeking and clean up feed layout

--- apps/web/src/app/feed/page.tsx
+++ apps/web/src/app/feed/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
-import {useQuery} from "@tanstack/react-query";
-import {webClientORPC} from "../../lib/orpc-web-client";
+import { useQuery } from "@tanstack/react-query";
+import { useQueryState } from "nuqs";
+import { webClientORPC } from "../../lib/orpc-web-client";
 import Header from "./_components/header";
 import Post from "./_components/post";
 import Search from "./_components/search-bar";
 
 export default function FeedPage() {
+  const [peeking, setPeeking] = useQueryState("peek", { defaultValue: "155" });
+
   const { data } = useQuery(
     webClientORPC.feed.getFeed.queryOptions({
       input: {},
@@ -22,7 +25,6 @@ export default function FeedPage() {
       <div className="flex justify-center">
         <Search />
       </div>
-      <p />
       <div className="flex items-center justify-center">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">{renderPosts}</div>
       </div>
