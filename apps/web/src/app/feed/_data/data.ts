75421e3a9429757a79281c2ace4042a4057c48aa
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-14 17:56:22 -0800

    feat: test post

--- apps/web/src/app/feed/_data/data.ts
+++ apps/web/src/app/feed/_data/data.ts
@@ -1,15 +1,13 @@
 import { internalPostSchema } from "@swapparel/contracts";
 
-export const data = internalPostSchema.parse([
-  {
-    _id: "e842525d-fe2d-4003-a88a-8e1c7da5d436",
-    createdBy: "test@example.com",
-    description: "Test post",
-    colour: "red",
-    size: "M",
-    material: ["canvas"],
-    images: ["https://example.com/image.jpg"],
-    hashtags: [],
-    qaEntries: [],
-  },
-]);
+export const data = internalPostSchema.parse({
+  _id: "e842525d-fe2d-4003-a88a-8e1c7da5d436",
+  createdBy: "test@example.com",
+  description: "Test post",
+  colour: "red",
+  size: "M",
+  material: ["canvas"],
+  images: ["https://static5.depositphotos.com/1039098/528/i/450/depositphotos_5288227-Happy-Indian-man-smiling.jpg"],
+  hashtags: [],
+  qaEntries: [],
+});
