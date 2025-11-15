fe04280f5d68d9c91a04b058b2319ad3fce3584b
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-14 18:53:11 -0800

    feat: skeleton feed

--- apps/web/src/app/feed/page.tsx
+++ apps/web/src/app/feed/page.tsx
@@ -2,6 +2,7 @@
 
 import { useQuery } from "@tanstack/react-query";
 import { webClientORPC } from "../../lib/orpc-web-client";
+import Header from "./_components/header";
 import Post from "./_components/post";
 
 export default function FeedPage() {
@@ -11,12 +12,12 @@ export default function FeedPage() {
       retry: false,
     })
   );
+  const renderPosts = data?.map((post) => <Post key={post._id} postData={post} />);
 
   return (
     <>
-      {data?.map((post) => (
-        <Post key={post._id} postData={post} />
-      ))}
+      <Header />
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">{renderPosts}</div>
     </>
   );
 }
