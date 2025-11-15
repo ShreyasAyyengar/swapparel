a9b4214617bbc80e0eccebbeec7f58e884ea5bc7
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-15 02:24:03 -0800

    chore(web): temp remove nuqs

--- apps/web/src/app/feed/page.tsx
+++ apps/web/src/app/feed/page.tsx
@@ -3,7 +3,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { webClientORPC } from "../../lib/orpc-web-client";
 import Header from "./_components/header";
-import Post from "./_components/post";
 import Search from "./_components/search-bar";
 
 // export const dynamic = "force-dynamic";
@@ -19,7 +18,7 @@ export default function FeedPage() {
       retry: false,
     })
   );
-  const renderPosts = data?.map((post) => <Post key={post._id} postData={post} onClickAction={setPeeking} />);
+  // const renderPosts = data?.map((post) => <Post key={post._id} postData={post} onClickAction={setPeeking} />);
 
   // TODO: customize scroll bar
   return (
@@ -29,9 +28,9 @@ export default function FeedPage() {
       <div className="flex justify-center">
         <Search />
       </div>
-      <div className="flex items-center justify-center">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">{renderPosts}</div>
-      </div>
+      {/*<div className="flex items-center justify-center">*/}
+      {/*  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">{renderPosts}</div>*/}
+      {/*</div>*/}
     </>
   );
 }
