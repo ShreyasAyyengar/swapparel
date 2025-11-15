f6058dcc9ba0e4a86797d3e62e4e14cbe393214f
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-14 19:11:59 -0800

    feat: images in header

--- apps/web/src/app/feed/_components/header.tsx
+++ apps/web/src/app/feed/_components/header.tsx
@@ -1,3 +1,11 @@
+import { SlidersHorizontal } from "lucide-react";
+import Image from "next/image";
+
 export default function Header() {
-  return <header>Header is Here</header>;
+  return (
+    <header className="m-4 flex justify-center">
+      <Image src="/simple-banner-slim.png" alt={""} width={200} height={10} />
+      <SlidersHorizontal />
+    </header>
+  );
 }
