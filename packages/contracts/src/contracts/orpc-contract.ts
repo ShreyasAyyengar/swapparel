6362f7fbce54a166ba5a76c5621b1279ca0344f2
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-14 16:52:44 -0800

    feat(web,contracts): add feed logic and integrate feed component into FeedPage

--- packages/contracts/src/contracts/orpc-contract.ts
+++ packages/contracts/src/contracts/orpc-contract.ts
@@ -1,6 +1,8 @@
+import { feedContract } from "./feed-contract.ts";
 import { postContract } from "./post-contract";
 
 export const contract = {
   // route URL: contract handler
   posts: postContract,
+  feed: feedContract,
 };
