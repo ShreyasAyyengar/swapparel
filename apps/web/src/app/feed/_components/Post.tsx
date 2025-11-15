564dcddaf1b1df3aa6c9198ed625963f87c91c6d
SerBakon <134323205+SerBakon@users.noreply.github.com>
2025-11-14 17:20:20 -0800

    feat: post skeleton

--- apps/web/src/app/feed/_components/Post.tsx
+++ apps/web/src/app/feed/_components/Post.tsx
@@ -1,4 +1,12 @@
-export default function Post() {
+import type { internalPostSchema } from "@swapparel/contracts";
+import Image from "next/image";
+import type z from "zod";
+
+export default function Post({ postData }: { postData: z.infer<typeof internalPostSchema> }) {
   // use effect
-  return <h1>POST</h1>;
+  return (
+    <button type="button">
+      <Image src={postData.images[0]} alt="thumbnail" />
+    </button>
+  );
 }
