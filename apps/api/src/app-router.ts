e1ce2dc165595384f8712bf0dec96cbc3ad8027b
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-11-16 14:30:51 -0800

    feat(api): add type support to app-router

--- apps/api/src/app-router.ts
+++ apps/api/src/app-router.ts
@@ -1,8 +1,10 @@
+import { implement } from "@orpc/server";
 import { feedRouter } from "./core/feed/feed-router";
 import { postRouter } from "./core/post/post-router";
+import type { AuthContext } from "./libs/http-context";
+import { contract } from "@swapparel/contracts";
 
-export const appRouter = {
-  // routeURL: route handlers
+export const appRouter = implement(contract).$context<AuthContext>().router({
   posts: postRouter,
   feed: feedRouter,
-};
+});
