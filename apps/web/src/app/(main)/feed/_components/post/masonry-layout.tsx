7b9b721266796d4cc7afdc02476a294d875e21c9
SerBakon <serbakonyt@gmail.com>
2025-11-27 23:28:57 -0800

    feat: masonry layout

---
+++ apps/web/src/app/(main)/feed/_components/post/masonry-layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import useMasonry from "../../_hooks/state/use-masonry";
+
+export default function MasonryLayout({ children }: { children: React.ReactNode }) {
+  const masonryRef = useMasonry();
+  return (
+    <div ref={masonryRef} className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+      {children}
+    </div>
+  );
+}
