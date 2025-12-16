8d0f40d95d057c85773d15c4ce561d0cd9bec914
SerBakon <serbakonyt@gmail.com>
2025-12-16 02:59:42 -0800

    fix: post images now scale with post outline

--- apps/web/src/app/(main)/feed/_components/filters/filter-layer.tsx
+++ apps/web/src/app/(main)/feed/_components/filters/filter-layer.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
+import { Masonry } from "@mui/lab";
 import { feedFilterSchema, filterPosts, type internalPostSchema } from "@swapparel/contracts";
-import Image from "next/image";
 import { parseAsBoolean, parseAsNativeArrayOf, parseAsString, useQueryState } from "nuqs";
 import { useMemo } from "react";
 import type { z } from "zod";
+import MasonryPost from "../post/masonry-post";
 
 export default function FilterLayer({ data }: { data: { posts: z.infer<typeof internalPostSchema>[] } }) {
   const [selectedColor] = useQueryState("colour", parseAsNativeArrayOf(parseAsString));
@@ -42,21 +42,21 @@ export default function FilterLayer({ data }: { data: { posts: z.infer<typeof in
 
   return (
     <div className="mt-15 mr-25 ml-25 flex items-center justify-center">
-      {/*<Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }} spacing={{ xs: 2, sm: 2, md: 2 }} sequential={true}>*/}
-      {/*  {filteredPosts.map((post) => (*/}
-      {/*    <Post key={post._id} postData={post} />*/}
-      {/*    // <Image src={post.images[0] ?? ""} alt="thumbnail" width={200} height={200} className="rounded-md border-2 border-[#6F4D3880]" />*/}
-      {/*  ))}*/}
-      {/*</Masonry>*/}
-      <ImageList variant={"masonry"} cols={5}>
+      {/*<MasonryLayout posts={filteredPosts.length}>*/}
+      {/*    {filteredPosts.map((post) => (*/}
+      {/*        <Post key={post._id} postData={post} />*/}
+      {/*    ))}*/}
+      {/*</MasonryLayout>*/}
+      <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }} spacing={3} sequential={true}>
         {filteredPosts.map((post) => (
-          // <Post key={post._id} postData={post} />
-          <ImageListItem key={post._id}>
-            <Image src={post.images[0] ?? ""} alt="thumbnail" width={200} height={200} className="rounded-md border-2 border-[#6F4D3880]" />
-            <ImageListItemBar title={"HELLO"} position={"top"} />
-          </ImageListItem>
+          <MasonryPost key={post._id} postData={post} />
         ))}
-      </ImageList>
+      </Masonry>
+      {/*<ImageList variant={"masonry"} cols={5} gap={10}>*/}
+      {/*  {filteredPosts.map((post) => (*/}
+      {/*    <MasonryPost key={post._id} postData={post} />*/}
+      {/*  ))}*/}
+      {/*</ImageList>*/}
     </div>
   );
 }
---
+++ apps/web/src/app/(main)/feed/_components/post/masonry-post.tsx
@@ -0,0 +1,40 @@
+import type { internalPostSchema } from "@swapparel/contracts";
+import { Badge } from "@swapparel/shad-ui/components/badge";
+import Image from "next/image";
+import type z from "zod";
+import PostTrigger from "./post-trigger";
+
+export default function MasonryPost({ postData }: { postData: z.infer<typeof internalPostSchema> }) {
+  return (
+    <PostTrigger postId={postData._id}>
+      <p className="font-bold">{postData.title}</p>
+      <span title={postData.createdBy} className="w-[20ch] truncate text-foreground">
+        {postData.createdBy}
+      </span>
+      <Image src={postData.images[0] ?? ""} width={200} height={200} alt="thumbnail" className="w-full rounded-md border-2 border-[#6F4D3880]" />
+      <div className="flex flex-col items-center justify-center">
+        <p title={postData.size} className="w-[20ch] truncate text-left text-foreground">
+          Size: <Badge className="bg-foreground font-bold text-background">{postData.size}</Badge>
+        </p>
+        <p title={postData.colour.join(", ")} className="w-[20ch] truncate text-left text-foreground">
+          {/*Colors: {postData.colour.join(", ")}*/}
+          Color:{" "}
+          {postData.colour.map((color) => (
+            <Badge className="mr-1 bg-foreground font-bold text-background" key={color}>
+              {color}
+            </Badge>
+          ))}
+        </p>
+        <p title={postData.material.join(", ")} className="w-[20ch] truncate text-left text-foreground">
+          {/*Materials: {postData.material}*/}
+          Material:{" "}
+          {postData.material.map((mats) => (
+            <Badge className="mr-1 bg-foreground font-bold text-background" key={mats}>
+              {mats}
+            </Badge>
+          ))}
+        </p>
+      </div>
+    </PostTrigger>
+  );
+}
