0a629ba431680d06b171722a2c97e3541190a5e5
quiksramore <quiksramore@gmail.com>
2025-12-17 06:20:46 -0800

    feat: swap contract/ finish swap router

--- packages/contracts/src/contracts/orpc-contract.ts
+++ packages/contracts/src/contracts/orpc-contract.ts
@@ -1,10 +1,12 @@
 import { cndContract } from "./cdn-contract";
 import { feedContract } from "./feed-contract";
 import { postContract } from "./post-contract";
+import { swapContract } from "./swap-contract";
 
 export const contract = {
   // route URL: contract handler
-  posts: postContract,
-  feed: feedContract,
-  cdn: cndContract,
+    posts: postContract,
+    feed: feedContract,
+    cdn: cndContract,
+    swap: swapContract,
 };
