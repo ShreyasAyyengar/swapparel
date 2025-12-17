b26cb5d7313e4e1ad38681dab12a2e97139c7a29
quiksramore <quiksramore@gmail.com>
2025-12-17 13:36:13 -0800

    feat: mondified swap_router

--- apps/api/src/core/swap/swap-schema.ts
+++ apps/api/src/core/swap/swap-schema.ts
@@ -1,20 +1,21 @@
-import { internalSwapSchema } from "@swapparel/contracts";
-import { toMongooseSchema } from "mongoose-zod";
-import { z } from "zod";
-import { databaseConnection } from "../../database/database";
+import {internalSwapSchema} from "@swapparel/contracts";
+import {toMongooseSchema} from "mongoose-zod";
+import {z} from "zod";
+import {databaseConnection} from "../../database/database";
 
 const SwapSchemaMongooseZod = internalSwapSchema
 
 const SwapSchemaMongoose = toMongooseSchema(
-    SwapSchemaMongooseZod.mongoose({
-        schemaOptions: {
-            collection: "swaps",
-            versionKey: false,
-        },
-    })
+  SwapSchemaMongooseZod.mongoose({
+    schemaOptions: {
+      collection: "swaps",
+      versionKey: false,
+    },
+  })
 );
 
-export interface ISwapSchemaMongoose extends z.infer<typeof SwapSchemaMongooseZod> {}
+export interface ISwapSchemaMongoose extends z.infer<typeof SwapSchemaMongooseZod> {
+}
 
 export const SwapCollection = databaseConnection.model<ISwapSchemaMongoose>("swaps", SwapSchemaMongoose, "swaps");
 
