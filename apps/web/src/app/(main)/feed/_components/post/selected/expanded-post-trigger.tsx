b7ca08ee614d7df238617c64c20baa2bf13bd770
SerBakon <serbakonyt@gmail.com>
2025-12-21 22:26:00 -0800

    feat: arrows

--- apps/web/src/app/(main)/feed/_components/post/selected/expanded-post-trigger.tsx
+++ apps/web/src/app/(main)/feed/_components/post/selected/expanded-post-trigger.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import type { internalPostSchema } from "@swapparel/contracts";
+import { ChevronRight } from "lucide-react";
 import Image from "next/image";
 import { useQueryState } from "nuqs";
 import { parseAsString } from "nuqs/server";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import type z from "zod";
 
 export default function ExpandedPostTrigger({ post, children }: { post: z.infer<typeof internalPostSchema>; children: React.ReactNode }) {
   const [_, setSelectedPost] = useQueryState("post", parseAsString);
+  const [currentImage, setCurrentImage] = useState<number>(0);
+  const [isHovered, setHovered] = useState<boolean>(false);
 
   const handleClose = async () => {
     await setSelectedPost(null);
@@ -23,9 +26,13 @@ export default function ExpandedPostTrigger({ post, children }: { post: z.infer<
     <div className="fixed inset-0 z-2 flex items-center justify-center">
       <button type="button" className="absolute inset-0 bg-black/30 backdrop-blur-sm" onMouseDown={handleClose} />
       <div className="z-10 grid max-h-[83vh] w-1/2 grid-cols-1 gap-5 overflow-y-auto rounded-2xl border border-secondary bg-accent p-10 text-foreground xl:grid-cols-2">
-        <div className="flex max-h-[calc(83vh-80px)] items-center justify-center overflow-y-auto border border-lime-500">
-          <Image src={post.images[0]} width={1200} height={1200} alt={"expanded-image"} className="w-full" />
+        <div className={"relative"} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+          <div className="flex max-h-[calc(83vh-80px)] items-center justify-center overflow-y-auto rounded-md border-2 border-secondary">
+            <Image src={post.images[0] ?? ""} width={1200} height={1200} alt={"expanded-image"} className="w-full" />
+          </div>
+          <ChevronRight className="absolute right-3" size={12} />
         </div>
+
         <div className="flex max-h-[calc(83vh-80px)] w-full flex-col overflow-auto rounded-md border-2 border-secondary bg-accent p-2">
           {children}
         </div>
