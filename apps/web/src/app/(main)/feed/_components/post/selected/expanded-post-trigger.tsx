134ebcacac0dfc6e0759fccfb32eaa1043bd79c9
SerBakon <serbakonyt@gmail.com>
2025-12-21 22:14:19 -0800

    feat: expanded post is mobile friendly

--- apps/web/src/app/(main)/feed/_components/post/selected/expanded-post-trigger.tsx
+++ apps/web/src/app/(main)/feed/_components/post/selected/expanded-post-trigger.tsx
@@ -22,12 +22,12 @@ export default function ExpandedPostTrigger({ post, children }: { post: z.infer<
   return (
     <div className="fixed inset-0 z-2 flex items-center justify-center">
       <button type="button" className="absolute inset-0 bg-black/30 backdrop-blur-sm" onMouseDown={handleClose} />
-      <div className="z-10 grid max-h-[83vh] w-1/2 grid-cols-1 rounded-2xl border border-secondary bg-accent p-10 text-foreground sm:grid-cols-2">
-        <div className="flex h-full items-center justify-center overflow-y-auto border border-lime-500">
+      <div className="z-10 grid max-h-[83vh] w-1/2 grid-cols-1 gap-5 overflow-y-auto rounded-2xl border border-secondary bg-accent p-10 text-foreground xl:grid-cols-2">
+        <div className="flex max-h-[calc(83vh-80px)] items-center justify-center overflow-y-auto border border-lime-500">
           <Image src={post.images[0]} width={1200} height={1200} alt={"expanded-image"} className="w-full" />
         </div>
-        <div className="flex items-center justify-center">
-          <span>TEST2</span>
+        <div className="flex max-h-[calc(83vh-80px)] w-full flex-col overflow-auto rounded-md border-2 border-secondary bg-accent p-2">
+          {children}
         </div>
       </div>
     </div>
