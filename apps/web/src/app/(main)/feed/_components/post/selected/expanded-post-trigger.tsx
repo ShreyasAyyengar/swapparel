76cb97fecdd038633a896071e5f7879eea85084e
SerBakon <serbakonyt@gmail.com>
2025-12-21 22:51:53 -0800

    feat: expanded post tracks which post user is on

--- apps/web/src/app/(main)/feed/_components/post/selected/expanded-post-trigger.tsx
+++ apps/web/src/app/(main)/feed/_components/post/selected/expanded-post-trigger.tsx
@@ -25,8 +25,11 @@ export default function ExpandedPostTrigger({ post, children }: { post: z.infer<
   return (
     <div className="fixed inset-0 z-2 flex items-center justify-center">
       <button type="button" className="absolute inset-0 bg-black/30 backdrop-blur-sm" onMouseDown={handleClose} />
-      <div className="z-10 grid max-h-[83vh] w-1/2 grid-cols-1 items-center gap-5 overflow-y-auto rounded-2xl border border-secondary bg-accent p-10 text-foreground xl:grid-cols-2">
-        <div className={"relative border border-lime-500"} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+      <div className="relative grid max-h-[83vh] w-1/2 grid-cols-1 items-center gap-5 overflow-y-auto rounded-2xl border border-secondary bg-accent p-10 text-foreground xl:grid-cols-2">
+        <p className={"absolute bottom-3 left-10"}>
+          {currentImage + 1} / {post.images.length}
+        </p>
+        <div className={"relative"} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
           <div className="flex max-h-[calc(83vh-80px)] items-center justify-center overflow-y-auto rounded-md border-2 border-secondary">
             <Image
               src={post.images[currentImage] ?? ""}
@@ -36,23 +39,23 @@ export default function ExpandedPostTrigger({ post, children }: { post: z.infer<
               className="flex w-full items-center justify-center"
             />
           </div>
-          {isHovered && (
+          {isHovered && currentImage < post.images.length - 1 && (
             <ChevronRight
               className="absolute top-1/2 right-4 z-10 h-10 w-10 translate-y-[-50%] cursor-pointer rounded-full bg-white/20 p-2 backdrop-blur-sm"
               size={12}
+              onClick={() => setCurrentImage((prev) => prev + 1)}
             />
           )}
-          {isHovered && (
+          {isHovered && currentImage > 0 && (
             <ChevronLeft
               className="absolute top-1/2 left-4 z-10 h-10 w-10 translate-y-[-50%] cursor-pointer rounded-full bg-white/20 p-2 backdrop-blur-sm"
               size={12}
+              onClick={() => setCurrentImage((prev) => prev - 1)}
             />
           )}
         </div>
 
-        <div className="flex h-full max-h-[calc(83vh-80px)] w-full flex-col overflow-auto rounded-md border-2 border-secondary bg-accent p-2">
-          {children}
-        </div>
+        <div className="flex max-h-[calc(83vh-80px)] flex-col overflow-auto rounded-md border-2 border-secondary bg-accent p-2">{children}</div>
       </div>
     </div>
   );
