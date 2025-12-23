91fa02395b666c41dc704be0b95473c173dcefce
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2025-12-23 18:50:38 +0800

    feat(web): implement new not found page with improved UI and navigation options

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,3 +1,9 @@
+import { NotFound } from "./_components/not-found";
+
 export default function NotFoundPage() {
-  return <div className="pt-16">Not Found!</div>;
+  return (
+    <div className="mt-30">
+      <NotFound />
+    </div>
+  );
 }
