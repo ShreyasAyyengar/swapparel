94c2e595fa30e0db867350b1181502ca6abd8124
SerBakon <serbakonyt@gmail.com>
2025-12-28 17:07:40 -0800

    feat: trading section UI

--- apps/web/src/app/(main)/feed/_components/post/trading/trade.tsx
+++ apps/web/src/app/(main)/feed/_components/post/trading/trade.tsx
@@ -15,11 +15,12 @@ export default function TradingBox({ post, onClick }: trading) {
       <div className="relative max-h-[90vh] w-1/2 items-center overflow-y-auto rounded-2xl border border-secondary bg-accent p-10 pt-5 text-foreground">
         <p className={"mb-5 flex justify-center font-light text-2xl"}>TRADING POST</p>
         <div className={"grid w-full grid-cols-1 items-center gap-5 xl:grid-cols-2"}>
-          {/* FIRST IMAGE */}
+          {/* TRADING IMAGE */}
           <TradingImage images={post.images} />
-          {/* SECOND IMAGE */}
-          <div className={"flex min-h-150 rounded-md border-2 border-secondary"}>
-            <p>Choose your post</p>
+          {/* YOUR SECTION */}
+          <div className={"flex h-[calc(83vh-80px)] flex-col rounded-md border-2 border-secondary"}>
+            <p className={"my-3 flex justify-center font-bold"}>Choose your post</p>
+            <div className={"h-full border border-secondary"} />
           </div>
 
           {/*<TradingImage images={post.images} />*/}
