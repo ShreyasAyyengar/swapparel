ae78bd5a52ad8f908191aa3476d04f67e093b505
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2026-01-05 18:31:29 -0800

    refactor(feed): enhance comments UI with badges and improved styling

--- apps/web/src/app/(main)/feed/_components/post/selected/root-comment.tsx
+++ apps/web/src/app/(main)/feed/_components/post/selected/root-comment.tsx
@@ -1,13 +1,14 @@
-import type {singleCommentSchema} from "@swapparel/contracts";
-import {Reply} from "lucide-react";
-import type {z} from "zod";
+import type { singleCommentSchema } from "@swapparel/contracts";
+import { Badge } from "@swapparel/shad-ui/components/badge";
+import { Reply } from "lucide-react";
+import type { z } from "zod";
 
 export default function Comment({ comment }: { comment: z.infer<typeof singleCommentSchema> }) {
   return (
     <p className="rounded-md p-1">
-      <span className="">{comment.author}</span>
+      <Badge className="mr-2 bg-black/60">{comment.author}</Badge>
       <br />
-      <div className={"ml-5 cursor-pointer rounded-md p-1 hover:bg-accent-200 hover:backdrop-blur-md"}>
+      <div className={"cursor-pointer rounded-md p-1 hover:bg-accent-200 hover:backdrop-blur-md"}>
         <span className="font-normal"> {comment.comment}</span>
         <span className="flex items-center font-normal text-accent-400 text-xs">
           reply <Reply className={"ml-1 flex h-5 w-5 justify-end"} />
