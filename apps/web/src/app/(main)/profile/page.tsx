9fc6225f7ccc2bea4016dd7b09c02e0c9db3283f
SerBakon <134323205+SerBakon@users.noreply.github.com>
2026-01-08 02:37:47 -0800

    feat: profile page
    # Conflicts:
    #	apps/web/src/app/(main)/profile/page.tsx

--- apps/web/src/app/(main)/profile/page.tsx
+++ apps/web/src/app/(main)/profile/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import {useEffect, useState} from "react";
-import FilterButton from "../feed/_components/filters/filter-button";
 import MasonryElement from "../feed/_components/post/masonry-element";
 import MasonryLayout from "../feed/_components/post/masonry-layout";
 
@@ -33,53 +31,26 @@ export default function Page() {
     price: 65,
   };
 
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    requestAnimationFrame(() => setMounted(true));
-  }, []);
-
   return (
-    <div className="flex w-full flex-col items-center justify-center">
-      <div
-        className={`mt-5 grid grid-cols-1 justify-items-center overflow-hidden rounded-full border border-secondary bg-primary p-5 px-10 text-foreground transition-[width] duration-700 ease-out ${mounted ? "w-1/2" : "w-0"} md:grid-cols-2 md:justify-between md:justify-items-stretch`}
-      >
-        {mounted && (
-          <>
-            <Image
-              src="https://picsum.photos/200"
-              alt="profile picture"
-              width="100"
-              height="100"
-              className="mr-0 mb-5 rounded-full md:mr-10 md:mb-0"
-            />
-            <div className="flex flex-col items-center gap-2 md:items-end">
-              <p className="text-center font-bold text-2xl md:text-end">althlin@ucsc.edu</p>
-              <div className="flex w-3/4 flex-col rounded-md border-2 border-secondary px-2 py-1 font-light">
-                <div className="flex items-center justify-between">
-                  <p>12 Posts</p>
-                  <p>Rating: 3/5</p>
-                </div>
-                <div className="flex items-center justify-between">
-                  <p>14 Items Traded</p>
-                </div>
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-
-      <div className="relative mt-10 flex w-3/4 flex-col items-center justify-center gap-5 rounded-md border-2 border-foreground bg-accent">
-        <FilterButton className={"absolute top-5 left-10 z-1"} />
+    <div className="flex w-full items-center justify-center">
+      <div className="mt-10 flex w-3/4 flex-col items-center justify-center gap-5 rounded-md border-2 border-foreground bg-accent">
+        <div className="mt-5 flex items-center justify-center gap-20 rounded-full border border-secondary bg-primary p-5 text-foreground">
+          <Image src="https://picsum.photos/200" alt="profile picture" width="100" height="100" className={"mr-10 rounded-full"} />
+          <div className="flex flex-col gap-2">
+            <p className={"font-bold text-2xl"}>Alex Lin</p>
+            <p className={"border-2 border-secondary px-2 text-center font-light"}>12 posts</p>
+          </div>
+        </div>
+        <div className={"w-full border border-secondary"} />
         <div>
-          <p className={"mt-5 font-bold text-2xl"}>POSTS</p>
+          <p className={"font-bold text-2xl"}>POSTS</p>
           <div className="w-full border border-foreground" />
         </div>
         <div className={"flex w-full items-center justify-center px-10"}>
           <MasonryLayout>
             <MasonryElement className={"bg-primary"} postData={MOCK_INTERNAL_POST} />
-            <MasonryElement className={"bg-primary"} postData={MOCK_INTERNAL_POST} />
-            <MasonryElement className={"bg-primary"} postData={MOCK_INTERNAL_POST} />
+            <MasonryElement postData={MOCK_INTERNAL_POST} />
+            <MasonryElement postData={MOCK_INTERNAL_POST} />
           </MasonryLayout>
         </div>
       </div>
