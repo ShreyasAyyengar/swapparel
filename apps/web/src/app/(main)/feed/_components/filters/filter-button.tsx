85a130e9147b3239123c38b893ecaa41ae2a0c72
SerBakon <134323205+SerBakon@users.noreply.github.com>
2026-01-08 03:15:52 -0800

    feat: profile page

--- apps/web/src/app/(main)/feed/_components/filters/filter-button.tsx
+++ apps/web/src/app/(main)/feed/_components/filters/filter-button.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { SlidersHorizontal } from "lucide-react";
-import { useRef, useState } from "react";
+import {cn} from "@swapparel/shad-ui/lib/utils";
+import {SlidersHorizontal} from "lucide-react";
+import {useRef, useState} from "react";
 import FilterOptions from "./filter-options";
 
-export default function FilterButton() {
+export default function FilterButton({ className = "fixed" }: { className: string }) {
   const [showingFilters, setShowingFilters] = useState(false);
   const toggleFilterOptions = () => setShowingFilters((prev) => !prev);
 
   const sliderRef = useRef<SVGSVGElement | null>(null);
 
   return (
-    <div className="fixed">
+    <div className={cn("fixed", className)}>
       <SlidersHorizontal ref={sliderRef} size={37.5} className="hover:cursor-pointer" onClick={toggleFilterOptions} />
       <FilterOptions sliderRef={sliderRef} onClick={toggleFilterOptions} showingFilters={showingFilters} setShowingFilters={setShowingFilters} />
     </div>
