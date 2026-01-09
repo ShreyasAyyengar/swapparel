74244d96f3b18d0a7ea2e0feda34c034bb2bb33a
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2026-01-08 18:10:39 -0800

    feat(contracts): add getUser route with validation and error handling

--- packages/contracts/src/contracts/user-contract.ts
+++ packages/contracts/src/contracts/user-contract.ts
@@ -1,1 +1,37 @@
-export const userContract = {};
+import { oc } from "@orpc/contract";
+import { z } from "zod";
+
+export const zodUserSchema = z.object({
+  name: z.string(),
+  email: z.email(),
+  image: z.string().optional(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  restricted: z.boolean(),
+});
+
+export const userContract = {
+  getUser: oc
+    .route({
+      method: "GET",
+    })
+    .input(
+      z.object({
+        email: zodUserSchema.shape.email,
+      })
+    )
+    .output(zodUserSchema)
+    .errors({
+      NOT_FOUND: {
+        data: z.object({
+          message: z.string(),
+        }),
+      },
+      INTERNAL_SERVER_ERROR: {
+        data: z.object({
+          message: z.string(),
+          issues: z.array(z.any()).optional(),
+        }),
+      },
+    }),
+};
