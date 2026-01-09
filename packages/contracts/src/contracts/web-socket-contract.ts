503ed0c351545806246bbb0fde6c2bfb7830a7fa
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2026-01-08 17:32:18 -0800

    feat(contracts): add schema validation for watchingTransaction in webSocketContract

--- packages/contracts/src/contracts/web-socket-contract.ts
+++ packages/contracts/src/contracts/web-socket-contract.ts
@@ -1,1 +1,11 @@
-export const webSocketContract = {};
+import { oc } from "@orpc/contract";
+import { z } from "zod";
+import { transactionSchema } from "./transaction-contract";
+
+export const webSocketContract = {
+  watchingTransaction: oc.input(
+    z.object({
+      transactionId: transactionSchema.shape._id,
+    })
+  ),
+};
