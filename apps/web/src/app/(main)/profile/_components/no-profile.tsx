503176a8398b29bbbbeea5dc1d5a99e758802d1b
SerBakon <134323205+SerBakon@users.noreply.github.com>
2026-01-08 19:59:16 -0800

    feat: loading state and no profile page

---
+++ apps/web/src/app/(main)/profile/_components/no-profile.tsx
@@ -0,0 +1,31 @@
+import {User} from "lucide-react";
+import {useEffect, useState} from "react";
+
+export default function NoProfile() {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    requestAnimationFrame(() => setMounted(true));
+  }, []);
+
+  return (
+    <div className="flex w-full flex-col items-center justify-center">
+      <div
+        className={`mt-5 grid grid-cols-1 items-center justify-center overflow-hidden rounded-full border border-secondary bg-primary p-5 px-10 text-foreground transition-[width] duration-700 ease-out ${mounted ? "w-1/2" : "w-0"} md:grid-cols-2 md:justify-between md:justify-items-stretch`}
+      >
+        {mounted && (
+          <>
+            <div className="mb-5 flex h-25 w-25 items-center justify-center overflow-hidden rounded-full border border-secondary bg-accent md:mr-10 md:mb-0">
+              <User className={"h-15 w-15"} />
+            </div>
+            <p className="text-center font-bold text-2xl md:text-end">No Profile</p>
+          </>
+        )}
+      </div>
+
+      <div className="relative mt-10 flex h-[50vh] w-3/4 flex-col items-center justify-center gap-5 rounded-md border-2 border-foreground bg-accent">
+        <p>No Posts</p>
+      </div>
+    </div>
+  );
+}
