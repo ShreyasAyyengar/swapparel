6d5c926ba44966c01ac3674e8725808640bd3186
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2026-01-08 18:10:30 -0800

    feat(profile): add session handling and dynamic user profile data

--- apps/web/src/app/(main)/_components/header/profile-button.tsx
+++ apps/web/src/app/(main)/_components/header/profile-button.tsx
@@ -1,14 +1,25 @@
 "use client";
-import {User} from "lucide-react";
-import {useRouter} from "next/navigation";
-import {authClient} from "../../../../lib/auth-client";
+import { LoaderCircle, User } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { authClient } from "../../../../lib/auth-client";
 
 export default function ProfileButton() {
   const router = useRouter();
-  const { data: authData } = authClient.useSession();
+  const { data: authData, isPending } = authClient.useSession();
+
+  if (isPending)
+    return (
+      <LoaderCircle
+        width={37.5}
+        height={37.5}
+        className="animate-spin text-background duration-100 ease-in hover:scale-110 hover:cursor-pointer hover:text-primary-foreground dark:hover:text-primary"
+      />
+    );
+
+  if (!authData) return <div />; // TODO: sign in button
 
   const handleClick = () => {
-    const email = authData?.user?.email ?? "test@ucsc.edu";
+    const email = authData.user.email;
     router.push(`/profile?profile=${encodeURIComponent(email)}`); //TODO: encodeURI???
   };
   return (
