0264308512bf6ebc2fc7338f4adcd2f4eafc3112
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2026-06-02 22:59:20 -0700

    refactor(api): add comment service and router with placeholders

--- apps/api/src/routers/http-router.ts
+++ apps/api/src/routers/http-router.ts
@@ -1,3 +1,4 @@
+import { commentRouter } from "../core/comments/comment-router";
 import { feedRouter } from "../core/feed/feed-router";
 import { postRouter } from "../core/post/post-router";
 import { transactionRouter } from "../core/swap/transaction-router";
@@ -5,6 +6,7 @@ import { userRouter } from "../core/users/user-router";
 
 export const httpRouter = {
   posts: postRouter,
+  comments: commentRouter,
   feed: feedRouter,
   transaction: transactionRouter,
   users: userRouter,
