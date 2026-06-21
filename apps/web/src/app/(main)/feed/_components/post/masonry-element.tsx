5ccbfd2979b0a1832634953a215b96db6f9ceb30
SerBakon <serbakonyt@gmail.com>
2026-06-20 23:11:51 -0700

    refactor: removed unused components

--- apps/web/src/app/(main)/feed/_components/post/masonry-element.tsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react";
-import PostDialog from "../shadcn-post/post-dialog";
-import type { postSchema } from "@swapparel/contracts";
-import type z from "zod";
-
-function MasonryElement({ className, postData }: { className: string; postData: z.infer<typeof postSchema> }) {
-  return <PostDialog postData={postData} className={className} />;
-}
-export default memo(MasonryElement);
