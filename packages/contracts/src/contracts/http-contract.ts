b7459112549ed7efd9c5c4b52e959db873268651
quiksramore <quiksramore@gmail.com>
2026-07-02 21:12:42 -0700

    feat(contracts): added exports for reporting contracts

--- packages/contracts/src/contracts/http-contract.ts
+++ packages/contracts/src/contracts/http-contract.ts
@@ -2,9 +2,10 @@ import { commentContract } from "./http/comments/comment-contract";
 import { feedContract } from "./http/feed/feed-contract";
 import { postContract } from "./http/post/post-contract";
 import { ratingContract } from "./http/rating/rating-contract";
+import { postReportContract, userReportContract } from "./http/reporting/report-contract";
+import { s3Contract } from "./http/s3/s3-contract";
 import { transactionContract } from "./http/transaction/transaction-contract";
 import { userContract } from "./http/user/user-contract";
-import { s3Contract } from "./http/s3/s3-contract";
 
 export const httpContract = {
   // route URL: contract handler
@@ -12,6 +13,8 @@ export const httpContract = {
   comments: commentContract,
   feed: feedContract,
   ratings: ratingContract,
+  userReport: userReportContract,
+  postReport: postReportContract,
   transaction: transactionContract,
   users: userContract,
   s3: s3Contract,
