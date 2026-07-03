7c15595fba9091ecccdcda7b4d5074a51c891e49
quiksramore <quiksramore@gmail.com>
2026-07-02 20:13:59 -0700

    feat(contracts): added createReport for report contracts

--- packages/contracts/src/contracts/http/reporting/report-contract.ts
+++ packages/contracts/src/contracts/http/reporting/report-contract.ts
@@ -1,8 +1,8 @@
 import { oc } from "@orpc/contract";
 import { z } from "zod";
-import { postReportSchema, userReportSchema } from "./report-schemas";
+import { userReportSchema } from "./report-schemas";
 
-export const userReportContract = {
+export const createReport = {
   createReport: oc
     .route({
       method: "POST",
@@ -10,80 +10,6 @@ export const userReportContract = {
     .input(
       userReportSchema.pick({
         reportedUserId: true,
-        reason: true,
-        description: true,
-      })
-    )
-    .output(
-      z.object({
-        id: z.uuidv7(),
-      })
-    )
-    .errors({
-      NOT_FOUND: {
-        data: z.object({
-          message: z.string(),
-        }),
-      },
-      CONFLICT: {
-        data: z.object({
-          message: z.string(),
-          issues: z.array(z.any()).optional(),
-        }),
-      },
-      INTERNAL_SERVER_ERROR: {
-        data: z.object({
-          message: z.string(),
-        }),
-      },
-    }),
-};
-
-export const postReportContract = {
-  createReport: oc
-    .route({
-      method: "POST",
-    })
-    .input(
-      postReportSchema.pick({
-        reportedPostId: true,
-        reason: true,
-        description: true,
-      })
-    )
-    .output(
-      z.object({
-        id: z.uuidv7(),
-      })
-    )
-    .errors({
-      NOT_FOUND: {
-        data: z.object({
-          message: z.string(),
-        }),
-      },
-      CONFLICT: {
-        data: z.object({
-          message: z.string(),
-          issues: z.array(z.any()).optional(),
-        }),
-      },
-      INTERNAL_SERVER_ERROR: {
-        data: z.object({
-          message: z.string(),
-        }),
-      },
-    }),
-};
-
-export const createPostReport = {
-  createReport: oc
-    .route({
-      method: "POST",
-    })
-    .input(
-      postReportSchema.pick({
-        reportedPostId: true,
         reason: true,
         description: true,
       })
