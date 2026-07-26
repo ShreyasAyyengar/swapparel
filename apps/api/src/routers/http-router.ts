a0b5f13f8c26f847b602703613d951853cf54dc7
quiksramore <quiksramore@gmail.com>
2026-07-26 13:47:10 -0700

    feat(api): updated http router

--- apps/api/src/routers/http-router.ts
+++ apps/api/src/routers/http-router.ts
@@ -6,12 +6,15 @@ import { ratingRouter } from "../core/rating/rating-router";
 import { s3Router } from "../core/s3/s3-router";
 import { transactionRouter } from "../core/swap/transaction-router";
 import { userRouter } from "../core/users/user-router";
+import {postReportRouter, userReportRouter} from "../core/reporting/report-router";
 
 export const httpRouter = {
   posts: postRouter,
   comments: commentRouter,
   feed: feedRouter,
   ratings: ratingRouter,
+  postReport: postReportRouter,
+  userReport: userReportRouter,
   transaction: transactionRouter,
   users: userRouter,
   s3: s3Router,
